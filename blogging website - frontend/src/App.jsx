e20c8b1b23cb23c1de5ed52cb8b9c84c70dfe84f
joshua-king <140848241+tfkjosh@users.noreply.github.com>
2025-04-18 13:23:19 -0500

    new project

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import Navbar from "./components/navbar.component";
+import { Routes, Route } from "react-router-dom";
+import UserAuthForm from "./pages/userAuthForm.page";
+
 const App = () => {
     return (
-        <h1>MERN Blogging website by modern web</h1>
-    )
+        //setting up routes
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="signin" element={<UserAuthForm type="sign-in" />} />
+                <Route path="signup" element={<UserAuthForm type="sign-up" />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
