de97ae6a9442eb94e647f003f46c766f261a7233
joshua-king <140848241+tfkjosh@users.noreply.github.com>
2025-04-30 00:21:41 -0500

    publish/ blog route

--- blogging website - frontend/src/components/publish-form.component.jsx
+++ blogging website - frontend/src/components/publish-form.component.jsx
@@ -2,10 +2,12 @@ import { Toaster, toast } from "react-hot-toast";
 import AnimationWrapper from "../common/page-animation";
 import { useContext } from "react";
 import { EditorContext } from "../pages/editor.pages";
+import Tag from "./tags.component";
 
 const PublishForm = () => {
 
     let characterLimit = 200;
+    let tagLimit = 10;
 
     let { blog, blog: { banner, title, tags, des }, setEditorState, setBlog } = useContext(EditorContext);
     
@@ -22,7 +24,31 @@ const PublishForm = () => {
     const handleBlogDesChange = (e) => {
         let input = e.target;
 
-        setBlog({...blog, des: input.value})
+        setBlog({...blog, des: input.value })
+    }
+
+    const handleTitleKeyDown = (e) => {
+        if(e.keyCode == 13) { //enter key
+            e.preventDefault();
+        }
+    }
+
+    const handleKeyDown = (e) => {
+        if(e.keyCode == 13 || e.keyCode == 188){
+            e.preventDefault();
+
+            let tag = e.target.value;
+
+            if(tags.length < tagLimit){
+                if(!tags.includes(tag) && tag.length){
+                    setBlog({...blog, tags: [ ...tags, tag] })
+                }
+            } else {
+                toast.error(`You can add max ${tagLimit} Tags`)
+            }
+
+            e.target.value = "";
+        } 
     }
 
     return (
@@ -61,9 +87,35 @@ const PublishForm = () => {
                     defaultValue={des}
                     className="h-40 resize-none leading-7 input-box pl-4"
                     onChange={handleBlogDesChange}
+                    onKeyDown={handleTitleKeyDown}
                 >
 
                 </textarea>
+
+                <p className="mt-1 text-dark-grey text-sm text-right">{ characterLimit - des.length } characters left</p>
+                <p className="text-dark-grey mb-2 mt-9">Topics - ( content )</p>
+
+                <div className="relative input-box pl-2 py-2 pb-4">
+                    <input type="text" placeholder="Topic" 
+                        className="sticky input-box bg-white top-0 left-0  pl-4 mb-3 focus:bg-white"
+                        onKeyDown={handleKeyDown}
+                    />
+                    
+                    { 
+                        tags.map((tag, i) => {
+                            return <Tag tag={tag} tagIndex={i}key={i} 
+                        />
+
+                    })}
+
+                </div>
+
+                <p className="mt-1 mb-4 text-dark-grey text-right">{ tagLimit - tags.length } Tags left</p>
+
+                <button className="btn-dark px-8">
+                    Publish
+                </button>
+
             </div>
 
         </section>
